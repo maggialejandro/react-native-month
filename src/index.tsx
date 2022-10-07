098474c698ebdb47272f4bae6d9a752a6f400ea7
Alejandro Maggi <maggi.alejandro@belonghome.com>
2022-10-07 10:10:08 -0300

    feat: export getWeekdayNames and getMonthNames functions

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
 export { default as Month } from './components/Month';
 
+export { getWeekdayNames, getMonthNames } from './utils/date';
+
 export { MonthProps, ThemeType, LocaleType, MarkedDays } from './types';
